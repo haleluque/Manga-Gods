87734e09d4cc6a7fae5a3e4a6946f4d36cf53a46
henry alejandro luque gomez <henry-luque@hotmail.com>
2017-04-04 14:29:31 -0500

    Se mueve el menú a la parte izquiera de la página principal

--- MangaGods/MangaGods/Site.Master.designer.cs
+++ MangaGods/MangaGods/Site.Master.designer.cs
@@ -13,15 +13,6 @@ namespace MangaGods {
     public partial class SiteMaster {
         
         /// <summary>
-        /// UpdatePanel control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.UpdatePanel UpdatePanel;
-        
-        /// <summary>
         /// adminAutores control.
         /// </summary>
         /// <remarks>
